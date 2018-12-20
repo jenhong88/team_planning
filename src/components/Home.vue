5e61b3426c9d4cd5219473a4b2b9f10d50ca4136
Kangil Lee <klee0589@gmail.com>
2018-12-20 00:54:20 -0500

    added axios

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,35 @@
 <template>
   <div class="hello">
-    <Header/>
+    <header />
     <h1>{{ msg }}</h1>
+    <div>{{ posts }}</div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Calendar',
   data () {
     return {
-      msg: 'WELCOME TO TEAMDREAM'
+      msg: 'WELCOME TO TEAMDREAM',
+      posts: [],
+      errors: []
     }
+  },
+  created() {
+    axios.get(`https://data.ny.gov/resource/5xaw-6ayf.json`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style scoped></style>
